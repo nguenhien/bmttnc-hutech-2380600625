864524fa334930995a8a8223f27ee17a77ccd3ef
nguenhien2552 <nguenhien2552@gmail.com>
2026-06-07 19:16:37 +0700

    Update lab-03

---
+++ lab-03/vigenere_cipher.py
@@ -0,0 +1,106 @@
+import sys
+import requests
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QMessageBox
+)
+
+from ui.vigenere import Ui_MainWindow
+
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
+        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/vigenere/encrypt"
+
+        payload = {
+            "plain_text": self.ui.txt_plain_text.toPlainText(),
+            "key": self.ui.txt_key.text().strip()
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+
+            if response.status_code == 200:
+                data = response.json()
+
+                self.ui.txt_cipher_text.setPlainText(
+                    data["encrypted_text"]
+                )
+
+                QMessageBox.information(
+                    self,
+                    "Success",
+                    "Encrypted Vigenere Successfully"
+                )
+
+            else:
+                QMessageBox.warning(
+                    self,
+                    "API Error",
+                    f"Status Code: {response.status_code}\n\n{response.text}"
+                )
+
+        except requests.exceptions.RequestException as e:
+            QMessageBox.critical(
+                self,
+                "Connection Error",
+                str(e)
+            )
+
+    def call_api_decrypt(self):
+        url = "http://127.0.0.1:5000/api/vigenere/decrypt"
+
+        payload = {
+            "cipher_text": self.ui.txt_cipher_text.toPlainText(),
+            "key": self.ui.txt_key.text().strip()
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+
+            if response.status_code == 200:
+                data = response.json()
+
+                self.ui.txt_plain_text.setPlainText(
+                    data["decrypted_text"]
+                )
+
+                QMessageBox.information(
+                    self,
+                    "Success",
+                    "Decrypted Vigenere Successfully"
+                )
+
+            else:
+                QMessageBox.warning(
+                    self,
+                    "API Error",
+                    f"Status Code: {response.status_code}\n\n{response.text}"
+                )
+
+        except requests.exceptions.RequestException as e:
+            QMessageBox.critical(
+                self,
+                "Connection Error",
+                str(e)
+            )
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MyApp()
+    window.show()
+
+    sys.exit(app.exec_())
